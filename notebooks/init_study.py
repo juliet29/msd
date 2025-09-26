0d864db645b8298a69e730d8d067bf6913c6317c
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2025-09-26 08:20:17 -0700

    iterate over plans and view what they look like

--- notebooks/init_study.py
+++ notebooks/init_study.py
@@ -8,15 +8,88 @@ app = marimo.App(width="medium")
 def _():
     from msd.main import access_dataset, sample_dataset,get_sample_dataset_with_rectangular_units
     import polars as pl 
-    import shapely as sh
+    import shapely 
     import math 
-    return (get_sample_dataset_with_rectangular_units,)
+    return get_sample_dataset_with_rectangular_units, pl, shapely
 
 
 @app.cell
 def _(get_sample_dataset_with_rectangular_units):
     kf = get_sample_dataset_with_rectangular_units()
     kf
+    return (kf,)
+
+
+@app.cell
+def _():
+ 
+    return
+
+
+@app.cell
+def _(kf, pl, shapely):
+    plans = []
+    plan_lists = []
+    names = []
+    for name, data in kf.sort(by="unit_id").group_by("unit_id"):
+        data = data.filter((pl.col("entity_type")== "area") & (pl.col("entity_subtype")!= "BALCONY") )
+        geom_list = data["geom"].to_list()
+        polys = [shapely.from_wkt(i) for i in geom_list]
+        res = shapely.MultiPolygon(polys)
+        plans.append(res)
+        plan_lists.append(polys)
+        names.append(name)
+    
+    
+    return names, plan_lists, plans
+
+
+@app.cell
+def _(names):
+    print(names)
+    return
+
+
+@app.cell
+def _(plan_lists):
+    plan_lists[0]
+    return
+
+
+@app.cell
+def _(plans):
+    plan = plans[0]
+    plan
+    return (plan,)
+
+
+@app.cell
+def _(plan, shapely):
+    union = shapely.unary_union(plan)
+    union
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _(plans):
+    plans[1]
+    return
+
+
+@app.cell
+def _(plans):
+    plans[2]
+    return
+
+
+@app.cell
+def _(plans):
+    plans[11]
     return
 
 
